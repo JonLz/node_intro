50571c977cfe2736de72ffca5613f889c8b94933
JonLz <jon.lazar@gmail.com>
2013-01-03 11:25:46 -0500

    Add request handling

---
+++ requestHandlers.js
@@ -0,0 +1,10 @@
+function start() {
+  console.log("Request for /start received")
+}
+
+function upload() {
+  console.log("Request for /upload received")
+}
+
+exports.start = start
+exports.upload = upload
